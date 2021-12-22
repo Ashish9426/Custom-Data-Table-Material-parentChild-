815a55cdf2ca1fef2b7e453819f8f4852e69b0bd
Ashish Mishra <ashishmishra9426@gmail.com>
2021-12-22 12:13:12 +0530

    update

--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 
 @Component({
@@ -7,26 +10,34 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
 
   @Input() dataSourceChild: any;
   @Input() colHeaderChild: any;
 
   dispColumns: any[] 
+  dataSource: any;
+
+  
 
   constructor() { }
 
   ngOnInit(): void {
   this.dispColumns = this.colHeaderChild.map(col=> col.col);
+  this.dataSource = new MatTableDataSource(this.dataSourceChild);
+  console.log(this.dataSource.data)
 
-  console.log(this.dispColumns)
-  console.log(this.colHeaderChild)
-  
+  // console.log(this.dispColumns)
+  // console.log(this.colHeaderChild)
+  // for(var i=0; i<this.colHeaderChild.length; i++){
+  //   console.log(this.colHeaderChild[i].col)
+  //   console.log(this.colHeaderChild[i].color)
+  // }
+}
 
-  for(var i=0; i<this.colHeaderChild.length; i++){
-    console.log(this.colHeaderChild[i].col)
-    console.log(this.colHeaderChild[i].color)
-  }
-  
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,6 +15,8 @@ export interface Employee {
 })
 export class HomeComponent implements OnInit {
 
+  iconsParent:any[] = ['edit', 'remove_red_eye', 'delete']
+
   employeeData: Employee[] = [
     {id:1, name:"Ashish", salary:15000, age:21},
     {id:2, name:"Amit", salary:23000, age:23},
@@ -26,21 +28,50 @@ export class HomeComponent implements OnInit {
     {id:8, name:"Manish", salary:40000, age:32},
     {id:9, name:"Sonal", salary:45000, age:30},
     {id:10, name:"Kritika", salary:85000, age:29},
+    // {id:11, name:"Ashish", salary:15000, age:21},
+    // {id:12, name:"Amit", salary:23000, age:23},
+    // {id:13, name:"Manish", salary:35000, age:35},
+    // {id:14, name:"Sonal", salary:45000, age:42},
+    // {id:15, name:"Kritika", salary:85000, age:25},
+    // {id:16, name:"Ashish", salary:27000, age:26},
+    // {id:17, name:"Amit", salary:23000, age:29},
+    // {id:18, name:"Manish", salary:40000, age:32},
+    // {id:19, name:"Sonal", salary:45000, age:30},
+    // {id:20, name:"Kritika", salary:85000, age:29},
+    // {id:21, name:"Ashish", salary:15000, age:21},
+    // {id:22, name:"Amit", salary:23000, age:23},
+    // {id:23, name:"Manish", salary:35000, age:35},
+    // {id:24, name:"Sonal", salary:45000, age:42},
+    // {id:25, name:"Kritika", salary:85000, age:25},
+    // {id:26, name:"Ashish", salary:27000, age:26},
+    // {id:27, name:"Amit", salary:23000, age:29},
+    // {id:28, name:"Manish", salary:40000, age:32},
+    // {id:29, name:"Sonal", salary:45000, age:30},
+    // {id:30, name:"Kritika", salary:85000, age:29},
+    // {id:31, name:"Ashish", salary:15000, age:21},
+    // {id:32, name:"Amit", salary:23000, age:23},
+    // {id:33, name:"Manish", salary:35000, age:35},
+    // {id:34, name:"Sonal", salary:45000, age:42},
+    // {id:35, name:"Kritika", salary:85000, age:25},
+    // {id:36, name:"Ashish", salary:27000, age:26},
+    // {id:37, name:"Amit", salary:23000, age:29},
+    // {id:38, name:"Manish", salary:40000, age:32},
+    // {id:39, name:"Sonal", salary:45000, age:30},
+    // {id:40, name:"Kritika", salary:85000, age:29},
   ];
 
   // displayedColumns: string[] = ['id', 'name', 'salary', 'age', 'action'];
 
-  iconsParent:any[] = ['edit', 'remove_red_eye', 'delete']
 
   displayedColumns: any[] =[
-    {col:'id', color:"accent",type:'icon', icon: this.iconsParent},
-    {col:"name", color:"primary", type:'text'},
-    {col:"age", color:"warn", type:'text'},
-    {col:"salary", color:"accent", type:'icon', icon: this.iconsParent},
-    {col:"action", color:"primary", type:'icon', icon: this.iconsParent}
+    {col:'id', color:"accent",type:'text', icon: this.iconsParent},
+    {col:"name", color:"primary", type:'text', icon: this.iconsParent},
+    {col:"age", color:"warn", type:'input', icon: this.iconsParent},
+    {col:"salary", color:"primary", type:'text', icon: this.iconsParent},
+    {col:"action", color:"warn", type:'icon', icon: this.iconsParent}
   ];
 
-  dispColumns: any[] = this.displayedColumns.map(col => col.col);
+  // dispColumns: any[] = this.displayedColumns.map(col => col.col);
   dataSourceParent:any = this.employeeData;
 
   constructor() { }
